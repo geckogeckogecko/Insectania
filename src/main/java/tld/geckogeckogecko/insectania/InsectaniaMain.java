3bb3423cd4f7f87fb4efe0471ea8bddafc34155a
geckogeckogecko <lorna_harvey@hotmail.com>
2022-10-25 19:55:33 +0100

    Added necessary metadata - mod now compiles and runs in MC!

--- src/main/java/tld/geckogeckogecko/insectania/InsectaniaMain.java
+++ src/main/java/tld/geckogeckogecko/insectania/InsectaniaMain.java
@@ -1,43 +1,25 @@
 package tld.geckogeckogecko.insectania;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-import org.slf4j.Logger;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
 
 @Mod(InsectaniaMain.MOD_ID)
 public class InsectaniaMain {
-  public static final Logger LOGGER = (Logger) LogManager.getLogger();
-  public static final String MOD_ID = "insectania";
-  
-  public InsectaniaMain() {
-    final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-    
-    modEventBus.addListener(this::setup);
-  }
-  
-  private void setup(final FMLCommonSetupEvent event) {
-    
-  }
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "insectania";
+
+    public InsectaniaMain() {
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
 }
